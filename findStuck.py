1ab3bc11c6984128f238a8099e7473d475112829
crees00 <40740074+crees00@users.noreply.github.com>
2019-06-24 15:52:58 +0100

    functions linking URN and ratings
    all the old URNs aren't in the current URN col

--- findStuck.py
+++ findStuck.py
@@ -7,14 +7,20 @@ This is a temporary script file.
 
 import pandas as pd
 import numpy as np
+import re
 
+global df0
 folderPath = r"C:\Users\ONS_2\Documents\Chris\\" 
 df0 = pd.read_csv(folderPath + 'bigDFnoDups1.csv')
-print('Data loaded!')
+print('\nData loaded!')
 
 global ratingsDict
+global currentRatingsDict
+global oldURNs
+global dodgyURNs
 ratingsDict = {}
-blanks = [[],0,0,0,0] # [random others, cat1, cat2, cat3, cat4]
+currentRatingsDict = {}
+oldURNs, dodgyURNs = [],[]
 
 def addRatingToDict(row):
     ''' Look up overall effectiveness rating for that row.
@@ -22,20 +28,62 @@ def addRatingToDict(row):
     If overall effectiveness is not 1-4, append it to list in position 0'''
     URN = row['URN']
     cat = row['Overall effectiveness']
-    print(URN, cat)
     
     if URN not in ratingsDict.keys():
-        ratingsDict[URN] = blanks.copy()
-    print(ratingsDict[URN])
+        ratingsDict[URN] = [[],0,0,0,0] # [random others, cat1, cat2, cat3, cat4]
     
     try:
         ratingsDict[URN][cat] += 1
     except:
-        print(ratingsDict[URN])
-        print(ratingsDict[URN][0])
-        ratingsDict[URN][0].insert(0,cat)
-        print(ratingsDict[URN][0])
+        ratingsDict[URN][0].insert(0,row['Overall effectiveness'])
 
-    print(ratingsDict)
-df0.apply(addRatingToDict, axis=1)
+
+def addPreviousRatingsToDict(row):
+    '''Look up schools/academies that are currently open. See if they have
+    any predecessors and add the predecessor scores to those of the current
+    school/academy.
+    Dictionary contains an entry for each school/academy that is currently open
+    '''
+    pred = row['Predecessor School URN(s)']
+    predList = re.findall('[0-9]{4,7}',str(pred))
+    URN = row['URN']
+    if len(predList) >0:
+        for no in predList:
+            # Remove URN entry from dictionary
+            print(no)
+            try:
+                currentRatingsDict.pop(no)
+                print('popped')
+            except:
+                pass
+            finally:
+                
+                addPredRatings(row['URN'], no)
+                # Add to list of removed URNs so don't double count later
+                oldURNs.append((row['URN'],no))
         
+
+def addPredRatings(currURN, oldURN):
+    '''Add the inspection ratings under the old URN to the inspection
+    ratings for the current URN
+    Just updates the currentRatingsDict dictionary
+    '''
+    print('addPredRatings',currURN,oldURN)
+    if oldURN in df0['URN']:
+        # Check not double counting as multiple rows will have same URN combo
+        if (currURN,oldURN) not in oldURNs:
+            currentRatingsDict[currURN][0].append(ratingsDict[oldURN][0])
+            for cat in range(1,5):
+                currentRatingsDict[currURN][cat] += ratingsDict[oldURN][cat]
+    else:
+        dodgyURNs.append(oldURN)
+        print(oldURN,'Not in URN col')
+
+df0.apply(addRatingToDict, axis=1)
+currentRatingsDict = ratingsDict.copy()
+df0.apply(addPreviousRatingsToDict, axis=1)
+#
+#110191
+#118328
+#106780, 137524
+#106562, 106563, 134688, 138412
