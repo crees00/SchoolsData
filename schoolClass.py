b5929addc11b3bc86336125d50c36af4d23bddce
crees00 <40740074+crees00@users.noreply.github.com>
2019-07-19 19:32:33 +0100

    classes done
    scores 430 stuck

--- schoolClass.py
+++ schoolClass.py
@@ -8,7 +8,9 @@ import pandas as pd
 import numpy as np
 import datetime
 import re
+import setFolder as sf
 
+where = sf.where
 start = datetime.datetime.now()
 print(f'starting at {start}')
 
@@ -21,10 +23,13 @@ class School:
         self.inspections = [] # list of instances of Inspection
         self.predecessorURNs = []
         self.predecessors = []
+        self.status = 'closed'  # 'open' or 'closed'
         
     def checkIfInspNoIsInList(self,inspNo):
         return inspNo in self.inspNosForSchool            
-        
+    
+    def __str__(self):
+        return f"{self.getURN()} with {len(self.getInspections())} inspections: {[x.getCat() for x in self.getInspections()]}"
     
     def getURN(self):
         return self.URN
@@ -34,6 +39,12 @@ class School:
     
     def getInspections(self):
         return self.inspections
+    
+    def getStatus(self):
+        return self.status
+    
+    def setStatus(self, statusIn):
+        self.status = statusIn
     
     def addInspToSchool(self, insp):
         self.inspections.append(insp)
@@ -49,10 +60,10 @@ class School:
         else:
             self.nines.append(insp)
     
-    def addPredecessorURNs(self, predURN):
-        self.predecessorURNs.append(predURN)
+    def addPredecessorFromURN(self, predURN):
         try:
             self.predecessors.append(SchoolDict[predURN])
+            self.predecessorURNs.append(predURN)
         except KeyError:
             predsThatAreNotInDF.append(predURN)
         
@@ -62,7 +73,10 @@ class Inspection:
         #        self.date = date
         self.cat = cat
         self.inspNo = inspNo
-
+        
+    def __str__(self):
+        return f'Inspection {self.getInspNo()}: Cat {self.getCat()}'
+    
     def getURN(self):
         return self.URN
 
@@ -98,12 +112,12 @@ def assignInspectionsToSchools(inspList):
             SchoolDict[insp.getURN()].addInspToSchool(insp)
         except KeyError:
             SchoolDict[insp.getURN()] = School(insp.getURN()) 
-            
+    print('inspections assigned to schools')        
     return SchoolDict, allInspNos, dupInsps    
 
 
 
-def addPredecessorURNs(row):
+def addPredecessorURNsFromDF(row):
     global SchoolDict
     pred = row["Predecessor School URN(s)"]
     URN = int(row['URN'])
@@ -115,34 +129,79 @@ def addPredecessorURNs(row):
             if no == row["URN"]:
                 continue
             try:
-                SchoolDict[URN].addPredecessor(no)
+                SchoolDict[URN].addPredecessorFromURN(no)
             except KeyError:
                 SchoolDict[URN] =School(URN)
-                SchoolDict[URN].addPredecessor(no)
+                SchoolDict[URN].addPredecessorFromURN(no)
     return row
 
-def addPredecessorInspections(school, SchoolDict):
-    for URN in school.getPredecessors():
-        if len(SchoolDict[URN].getPredecessors())>0:
-            addPredecessorInspections(SchoolDict[URN])
-        for insp in SchoolDict[URN].getInspections:
-            if school.checkIfInspNoIsInList(inspNo) == False:
+def addPredecessorInspections(school):
+    for pred in school.getPredecessors():
+        if len(pred.getPredecessors())>0:
+            pred = addPredecessorInspections(pred)
+        for insp in pred.getInspections():
+            if school.checkIfInspNoIsInList(insp.getInspNo()) == False:
                 school.addInspToSchool(insp)
     return school
 
 def addAllPredecessors(SchoolDict):
     for URN in SchoolDict.keys():
-        SchoolDict[URN] = addPredecessorInspections(SchoolDict[URN], SchoolDict)
+        SchoolDict[URN] = addPredecessorInspections(SchoolDict[URN])
+    print('all predecessors added')
     return SchoolDict
+
+def calcStuck(SchoolDict):
+    stuck = []
+    for URN in SchoolDict.keys():
+        tally=[]
+        flag = False
+        for insp in SchoolDict[URN].getInspections():
+            cat = insp.getCat()
+            if (cat==1) or (cat==2):
+                flag = True
+                break
+            tally.append(cat)
+        if (len(tally) >=4) and (not flag):
+            stuck.append(SchoolDict[URN])
+    print('stuck schools found')
+    return stuck
+
+def setAllStatuses(SchoolDict):
+    folderPath = sf.folderPath
+    openAndUninspected = []
+    if where == "ONS":
+        file = "Data\edubaseallstatefunded20190704.csv"
+    else:
+        file = "edubaseallstatefunded20190627.csv"
+    openSchools = pd.read_csv(folderPath + file, encoding="latin-1")
+    openSchoolsSet = set(openSchools['URN'])
+    for URN in openSchoolsSet:
+        try:
+            SchoolDict[URN].setStatus('open')
+        except KeyError:
+            SchoolDict[URN] = School(URN)
+            SchoolDict[URN].setStatus('open')
+            openAndUninspected.append(SchoolDict[URN])
+    print('school statuses set')
+    return SchoolDict, openAndUninspected
+
+def whichStuckAreOpen(stuck):
+    openStuck = []
+    for school in stuck:
+        if school.getStatus() == 'open':
+            openStuck.append(school)
+    print(f'{len(openStuck)} open stuck schools')
+    return openStuck
 
 predsThatAreNotInDF = []
 inspList = []
 df = pd.read_csv("bigDFnoDups1.csv")
-#
+
 df = df.apply(loadInspections, axis=1)
 SchoolDict, allInspNos, dupInsps = assignInspectionsToSchools(inspList)
-df = df.apply(addPredecessorURNs, axis=1)
+df = df.apply(addPredecessorURNsFromDF, axis=1)
 SchoolDict = addAllPredecessors(SchoolDict)
-    
-
+stuck = calcStuck(SchoolDict)
+SchoolDict, openAndUninspected = setAllStatuses(SchoolDict)
+openStuck = whichStuckAreOpen(stuck)
 print(f'took {datetime.datetime.now()-start}')
